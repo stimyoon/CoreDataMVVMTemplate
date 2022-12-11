5ae03d63ff7d42efc997605dffdaf4633e71970e
stimyoon <64033604+stimyoon@users.noreply.github.com>
2022-12-11 04:39:39 -0500

    Initial Commit

---
+++ CoreDataMVVMTemplate/CoreDataMVVMTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMVVMTemplateApp.swift
+//  CoreDataMVVMTemplate
+//
+//  Created by Tim Yoon on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMVVMTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
